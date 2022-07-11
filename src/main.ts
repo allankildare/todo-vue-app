39ecbd32b3f683791a912131c33211953364f85c
Allan Kildare <allankildaredev@gmail.com>
2022-07-11 19:38:12 -0300

    chore: configuration of v-calendar and vue3-storage

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import Vue3Storage, { StorageType } from 'vue3-storage'
+import VCalendar from 'v-calendar'
 import 'ress'
+import 'v-calendar/dist/style.css'
 
 loadFonts()
 
 createApp(App)
+  .use(VCalendar, {})
+  .use(Vue3Storage, { namespace: 'pro_', storage: StorageType.Local })
   .use(vuetify)
   .mount('#app')
