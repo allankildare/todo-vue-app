d56707f245b9657f4f8b89deab0ac8519c11a2e5
Allan Kildare <allankildaredev@gmail.com>
2022-07-11 19:35:39 -0300

    feat: adding TaskModal component

---
+++ src/components/TaskModal/TaskModal.vue
@@ -0,0 +1,96 @@
+<template>
+  <transition name="modal-animation">
+    <div v-show="modalActive" class="task-modal">
+      <transition name="modal-animation-inner">
+        <div v-show="modalActive" class="modal-inner">
+            <!-- Close -->
+            <!-- modal content -->
+            <slot />
+            <button @click="close" type="button" class="close-button" title="Close tab">
+              <i class="mdi-close-circle mdi v-icon notranslate v-theme--light v-icon--size-large" aria-hidden="true"></i>
+            </button>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { VBtn } from 'vuetify/components'
+
+export default defineComponent({
+  name: "TaskModal",
+  props: ["modalActive"],
+
+  components: {
+    VBtn
+  },
+  
+  setup(props, { emit }) {
+    function close() {
+      emit('close')
+    }
+
+    return { close }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.modal-animation-enter-active,
+.modal-animation-leave-active {
+  transition: opacity .4s cubic-bezier(.52, .02, .19, 1.02)
+}
+
+.modal-animation-enter-from,
+.modal-animation-leave-to {
+  opacity: 0;
+}
+
+.modal-animation-inner-enter-active {
+  transition: all .4s cubic-bezier(.52, .02, .19, 1.02) .15s;
+}
+.modal-animation-inner-leave-active {
+  transition: all .4s cubic-bezier(.52, .02, .19, 1.02);
+}
+
+.modal-animation-inner-enter-from {
+  opacity: 0;
+  transform: scale(.8);
+}
+
+.modal-animation-inner-leave-to {
+  transform: scale(.8);
+}
+/*.modal-animation-leave-to {
+  opacity: 1;
+}*/
+
+.task-modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(20,20,20,.4);
+  
+
+  .modal-inner {
+    position: relative;
+    max-width: 640px;
+    width: 80%;
+    background-color: #fff;
+    padding: 4rem 1rem;
+  }
+
+  .close-button {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+  }
+}
+</style>
