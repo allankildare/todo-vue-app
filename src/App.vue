7e19e84070d13bf056b1a39aa2aece2bcb15d589
Allan Kildare <allankildaredev@gmail.com>
2022-07-12 02:40:35 -0300

    feat: first functional modal to add tasks

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,19 @@
       <h1>Add task</h1>
       <p>Only some information is needed to add a task, let's go?</p>
       <v-form ref="form">
-        <v-text-field v-model="model" :counter="max" label="Title" />
-        
-        <v-date-picker v-model="date" mode="dateTime" is24hr>
+        <v-text-field
+          label="Title"
+          v-model="taskForm.title"
+          :rules="[rules.required, rules.minLength, rules.maxLength]"
+          counter="50"
+          maxLength="50"
+        />
+
+        <v-date-picker v-model="taskForm.date" mode="dateTime" is24hr>
           <template v-slot="{ inputValue, inputEvents }">
             <v-text-field
-              v-model="todayDate"
+              v-model="taskForm.date"
+              :rules="[rules.required]"
               :counter="max"
               :value="inputValue"
               v-on="inputEvents"
@@ -35,16 +42,22 @@
           </template>
         </v-date-picker>
 
-        <v-textarea label="Description"/>
+        <v-textarea
+          v-model="taskForm.description"
+          label="Description"
+          :rules="[rules.required, rules.maxLength]"
+          counter="1000"
+          clearable
+        />
 
-        <v-btn color="primary">Add task</v-btn>
+        <v-btn color="primary" @click="submit">Add task</v-btn>
       </v-form>
     </div>
   </task-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, reactive, computed } from 'vue'
 import {
   VSnackbar,
   VBtn,
@@ -53,17 +66,18 @@ import {
   VInput,
   VForm,
   VTextField,
-  VTextarea
-} from "vuetify/components";
-import HelloWorld from "./components/HelloWorld.vue";
-import Tasks from "./components/Tasks/Tasks.vue";
-import TaskModal from "~/components/TaskModal/TaskModal.vue";
+  VTextarea,
+} from 'vuetify/components'
+import Tasks from './components/Tasks/Tasks.vue'
+import TaskModal from '~/components/TaskModal/TaskModal.vue'
+import { useStorage } from 'vue3-storage'
+import useVuelidate from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
 
   components: {
-    HelloWorld,
     VSnackbar,
     VBtn,
     VMain,
@@ -73,27 +87,104 @@ export default defineComponent({
     VInput,
     VForm,
     VTextField,
-    VTextarea
+    VTextarea,
   },
 
   data() {
     return {
       snackbar: false,
-      snackbarText: "Testando snackbar",
+      snackbarText: 'Testando snackbar',
       timeout: 2000,
-    };
+      rules: {
+        required: (value: string) => !!value || 'This field is required',
+        maxLength: (value: string) =>
+          (value || '').length <= 1000 ||
+          'Description must be 1000 characters or less.',
+        minLength: (value: string) =>
+          value.length > 2 || 'This field must have at least 2 characters',
+      },
+      taskForm: {
+        title: '',
+        date: '',
+        description: '',
+      },
+    }
   },
 
   setup() {
-    const taskModalActive = ref(false);
+    // pendencies
+    // 1. transform values into states
+    // 2. create computed rules
+    // const state = reactive({
+    //   title: '',
+    //   date: '',
+    //   description: '',
+    // })
+
+    // const rules = computed(() => {
+    //   const localRules = {
+    //     title: {
+    //       required,
+    //       minLength: minLength(2),
+    //     },
+    //     date: {
+    //       required,
+    //     },
+    //     description: {
+    //       required,
+    //       minLength: minLength(2),
+    //     },
+    //       required: (value: string) => !!value || 'This field is required',
+    //       counter: (value: string) => value.length
+
+    //   }
+
+    //   return localRules
+    // })
+
+    // const v$ = useVuelidate(rules, state)
+
+    const taskModalActive = ref(false)
 
     function toggleTaskModal() {
-      taskModalActive.value = !taskModalActive.value;
+      taskModalActive.value = !taskModalActive.value
     }
 
-    return { taskModalActive, toggleTaskModal };
+    return { taskModalActive, toggleTaskModal }
   },
-});
+
+  methods: {
+    async submit() {
+      const { title, date, description } = this.taskForm
+      const storage = useStorage()
+
+      const getAndSetStorage =
+        (await storage.getStorage({
+          key: 'tasks',
+          success: ({ data }) => {
+            const taskData = JSON.parse(data)
+
+            storage.setStorage({
+              key: 'tasks',
+              data: JSON.stringify([...taskData, { title, date, description }]),
+            })
+          },
+          fail: () => {
+            storage.setStorage({
+              key: 'tasks',
+              data: JSON.stringify([{ title, date, description }]),
+            })
+          }
+        }))
+
+        return getAndSetStorage
+      // pendencies
+      // 3. clear form
+      // this.$refs.form.reset()
+      // 4. validate the entire form
+    },
+  },
+})
 </script>
 
 <style lang="scss" scoped>
