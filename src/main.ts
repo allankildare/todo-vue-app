817802aa51513c984681a62d8157af45317ce9ae
Allan Kildare <allankildaredev@gmail.com>
2022-07-13 03:37:08 -0300

    chore: implementing pinia in the application

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
@@ -9,7 +10,10 @@ import 'v-calendar/dist/style.css'
 
 loadFonts()
 
+const pinia = createPinia()
+
 createApp(App)
+  .use(pinia)
   .use(VCalendar, {})
   .use(Vue3Storage, { namespace: 'todoapp_', storage: StorageType.Local })
   .use(vuetify)
