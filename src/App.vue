0f4d7cb8944f4e7af8224bc207f54043e24ae72a
Allan Kildare <allankildaredev@gmail.com>
2022-07-15 06:28:07 -0300

    feat: first complete to-do application :sparkles:

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,63 @@
 <template>
   <tasks />
-  <v-btn dark color="blue darken-2" @click="toggleTaskModal" class="new-task">
+  <v-btn
+    dark
+    color="blue darken-2"
+    class="new-task"
+    @click="toggleTaskModal"
+  >
     New task
   </v-btn>
-  <v-snackbar v-model="snackbar" :timeout="timeout">
-    {{ snackbarText }}
-    <template v-slot:action="{ attrs }">
-      <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+  <v-snackbar
+    v-model="snackbar.isActive"
+    :timeout="timeout"
+  >
+    {{ snackbar.text }}
+    <template #action="{ attrs }">
+      <v-btn
+        color="blue"
+        text
+        v-bind="attrs"
+        @click="snackbar = false"
+      >
         Close
       </v-btn>
     </template>
   </v-snackbar>
-  <task-modal :modalActive="taskModalActive" @close="toggleTaskModal">
+  <task-modal
+    :modal-active="taskModalActive"
+    @close="toggleTaskModal"
+  >
     <div class="task-modal-content">
       <h1>Add task</h1>
       <p>Only some information is needed to add a task, let's go?</p>
       <v-form ref="form">
         <v-text-field
-          label="Title"
           v-model="taskForm.title"
+          label="Title"
           :rules="[rules.required, rules.minLength, rules.maxLength]"
           counter="50"
-          maxLength="50"
+          max-length="50"
         />
 
-        <v-date-picker v-model="taskForm.date" mode="dateTime" is24hr>
-          <template v-slot="{ inputValue, inputEvents }">
+        <v-date-picker
+          v-model="taskForm.date"
+          mode="dateTime"
+          is24hr
+        >
+          <template #default="{ inputValue, inputEvents }">
             <v-text-field
               v-model="taskForm.date"
               :rules="[rules.required]"
               :counter="max"
               :value="inputValue"
-              v-on="inputEvents"
               title="Date to complete"
               single-line
               placeholder="Date to complete"
               prepend-inner-icon="mdi-calendar"
               readonly
-            >
-            </v-text-field>
+              v-on="inputEvents"
+            />
           </template>
         </v-date-picker>
 
@@ -49,21 +68,29 @@
           counter="1000"
           clearable
         />
+        <p
+          v-if="error.isError"
+          class="text-body-1 text-red"
+        >
+          {{ error.message }}
+        </p>
 
-        <v-btn color="primary" @click="submit">Add task</v-btn>
+        <v-btn
+          color="primary"
+          @click="submit"
+        >
+          Add task
+        </v-btn>
       </v-form>
     </div>
   </task-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, reactive, computed } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import {
   VSnackbar,
   VBtn,
-  VMain,
-  VApp,
-  VInput,
   VForm,
   VTextField,
   VTextarea,
@@ -71,9 +98,7 @@ import {
 import Tasks from './components/Tasks/Tasks.vue'
 import TaskModal from '~/components/TaskModal/TaskModal.vue'
 import { useTasksStore } from '~/stores/useTasksStore'
-// use vuelidate
-import useVuelidate from '@vuelidate/core'
-import { required, email, minLength } from '@vuelidate/validators'
+import { isTaskLate } from '~/helpers/validateDate'
 
 export default defineComponent({
   name: 'App',
@@ -81,20 +106,30 @@ export default defineComponent({
   components: {
     VSnackbar,
     VBtn,
-    VMain,
-    VApp,
     Tasks,
     TaskModal,
-    VInput,
     VForm,
     VTextField,
     VTextarea,
   },
 
   setup() {
-    // pendencies
-    // 1. transform values into states
-    // 2. create computed rules
+    const taskForm = reactive({
+      title: '',
+      date: '',
+      description: '',
+    })
+
+    const error = reactive({
+      isError: false,
+      message: '',
+    })
+
+    const errorText = {
+      required: 'All fields are required',
+      invalidDate: 'You should put a date in the future',
+      title: 'Title should have at least 2 characters',
+    }
 
     const tasksStore = useTasksStore()
 
@@ -104,14 +139,59 @@ export default defineComponent({
       taskModalActive.value = !taskModalActive.value
     }
 
-    return { taskModalActive, toggleTaskModal, tasksStore }
+    const snackbar = reactive({
+      isActive: false,
+      text: 'Task has been saved',
+    })
+
+    async function submit() {
+      const { title, date, description } = taskForm
+      const tasksStore = useTasksStore()
+
+      // validate
+      if (!title || !date || !description) {
+        error.isError = true
+        error.message = errorText.required
+      } else if (title.length < 2) {
+        error.isError = true
+        error.message = errorText.title
+      } else if (isTaskLate(date)) {
+        error.isError = true
+        error.message = errorText.invalidDate
+      } else {
+        // add to tasks store
+        await tasksStore.addTask({
+          title,
+          date,
+          description,
+        })
+        // clean form
+        taskForm.title = ''
+        taskForm.date = ''
+        taskForm.description = ''
+
+        // close modal
+        toggleTaskModal()
+
+        // trigger snackbar
+        snackbar.isActive = !snackbar.isActive
+      }
+    }
+
+    return {
+      error,
+      snackbar,
+      submit,
+      taskForm,
+      taskModalActive,
+      tasksStore,
+      timeout: 2000,
+      toggleTaskModal,
+    }
   },
 
   data() {
     return {
-      snackbar: false,
-      snackbarText: 'Testando snackbar',
-      timeout: 2000,
       rules: {
         required: (value: string) => !!value || 'This field is required',
         maxLength: (value: string) =>
@@ -120,37 +200,14 @@ export default defineComponent({
         minLength: (value: string) =>
           value.length >= 2 || 'This field must have at least 2 characters',
       },
-      taskForm: {
-        title: '',
-        date: '',
-        description: '',
-      },
     }
-  },
-
-  methods: {
-    async submit() {
-      const { title, date, description } = this.taskForm
-      const tasksStore = useTasksStore()
-
-      return tasksStore.addTask({
-        title,
-        date,
-        description,
-      })
-
-      // pendencies
-      // 3. clear form
-      // this.$refs.form.reset()
-      // 4. validate the entire form
-    },
   },
 })
 </script>
 
 <style lang="scss" scoped>
 .new-task {
-  position: absolute;
+  position: fixed;
   bottom: 2rem;
   left: 2rem;
 }
--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -10,24 +10,25 @@
         color="#d4d4d4"
         width="180px"
       >
-        <h1 class="text-h5">{{ type }}</h1>
+        <h1 class="text-h5 text-capitalize">
+          {{ type === 'incomplete' ? 'In progress' : capitalizeFirstLetter(type) }}
+        </h1>
       </v-card>
     </v-badge>
     <div class="tasks-list">
       <div
-        v-for="item in filteredTasks"
-        :key="item"
+        v-for="task in filteredTasks"
+        :key="task.id"
         class="task"
       >
-        <!-- TO DO: transform p tag in a Vuetify badge -->
         <p
-          v-if="item.status === 'complete'"
+          v-if="task.status === 'complete'"
           class="font-italic"
         >
           Completed
         </p>
         <p
-          v-else-if="item.status === 'incomplete' && !isTaskLate(item.date)"
+          v-else-if="task.status === 'incomplete' && !isTaskLate(task.date)"
           class="font-italic"
         >
           In progress
@@ -39,48 +40,133 @@
           Late
         </p>
         <h2 class="text-truncate">
-          {{ item.title }}
+          {{ task.title }}
         </h2>
-        <p>{{ item.description }}</p>
+        <p
+          class="task-description"
+          @click="openModal(task.id)"
+        >
+          {{ task.description }}
+        </p>
 
         <div class="d-flex flex-row justify-end align-center">
           <v-btn
-            v-if="item.status !== 'complete'"
+            v-if="task.status !== 'complete'"
             icon="mdi-check"
             color="success"
             size="small"
             title="Complete task"
             class="mr-2"
-            @click="tasksStore.completeTask(item.id)"
+            @click="tasksStore.completeTask(task.id)"
           />
-          
+
           <v-btn
             icon="mdi-delete"
             color="error"
             size="small"
             title="Delete task"
-            @click="tasksStore.deleteTask(item.id)"
+            @click="tasksStore.deleteTask(task.id)"
           />
         </div>
       </div>
+      <task-modal
+        :modal-active="taskModalActive"
+        @close="toggleTaskModal"
+      >
+        <div v-if="state.editable === false">
+          <h1>{{ taskForModal.title }}</h1>
+          <span
+            class="font-italic"
+            :title="formatDate(taskForModal.date)"
+          >
+            {{ timeFromNow(taskForModal.date) }}
+          </span>
+          <p>{{ taskForModal.description }}</p>
+          <v-btn
+            v-if="taskForModal.status !== 'complete'"
+            icon="mdi-pencil"
+            color="black"
+            size="small"
+            title="Edit task"
+            class="mr-2"
+            @click="editTask"
+          />
+        </div>
+
+        <div v-else-if="state.editable === true">
+          <v-form ref="form">
+            <h1>Edit task</h1>
+            <span class="font-italic">The current date of this task is:
+              <strong>{{ taskForModal.title }}</strong>
+            </span>
+            <v-text-field
+              v-model="state.taskEditForm.title"
+              label="Title"
+              counter="50"
+              max-length="50"
+            />
+
+            <span class="font-italic">
+              The current date of this task is:
+              {{ formatDate(taskForModal.date) }}
+            </span>
+
+            <v-date-picker
+              v-model="state.taskEditForm.date"
+              mode="dateTime"
+              is24hr
+            >
+              <template #default="{ inputValue, inputEvents }">
+                <v-text-field
+                  v-model="state.taskEditForm.date"
+                  :counter="max"
+                  :value="inputValue"
+                  title="Date to complete"
+                  single-line
+                  placeholder="Date to complete"
+                  prepend-inner-icon="mdi-calendar"
+                  readonly
+                  v-on="inputEvents"
+                />
+              </template>
+            </v-date-picker>
+
+            <v-textarea
+              v-model="state.taskEditForm.description"
+              label="Description"
+              counter="1000"
+              clearable
+            />
+
+            <v-btn
+              color="primary"
+              @click="submit(task.id)"
+            >
+              Edit task
+            </v-btn>
+          </v-form>
+        </div>
+      </task-modal>
     </div>
   </v-container>
 </template>
 
-<script>
-import { defineComponent, onMounted, reactive, computed } from 'vue'
+<script lang="ts">
+import { defineComponent, reactive, computed, ref } from 'vue'
+import { capitalizeFirstLetter } from '~/helpers/capitalizeFirstLetter'
+import TaskModal from '~/components/TaskModal/TaskModal.vue'
+
 import {
   VBadge,
   VCard,
-  VSnackbar,
   VBtn,
-  VMain,
-  VApp,
   VContainer,
-  VCheckbox,
-  VHover,
+  VTextField,
+  VForm,
+  VTextarea,
 } from 'vuetify/components'
-import { isTaskLate } from '~/helpers/validateDate'
+
+import { isTaskLate, timeFromNow, formatDate } from '~/helpers/validateDate'
 import { useTasksStore } from '~/stores/useTasksStore'
 
 export default defineComponent({
@@ -89,40 +175,107 @@ export default defineComponent({
     VBadge,
     VCard,
     VContainer,
-    VSnackbar,
     VBtn,
+    TaskModal,
+    VTextField,
+    VForm,
+    VTextarea,
   },
 
   props: {
     type: {
       type: String,
-      default: ''
+      default: '',
     },
   },
-  
 
   setup(props) {
+    const state = reactive({
+      editable: false,
+      taskEditForm: {
+        title: '',
+        date: '',
+        description: '',
+      },
+    })
+
+    const taskForModal = reactive({
+      title: '',
+      date: '',
+      description: '',
+      status: ''
+    })
+
     const tasksStore = useTasksStore()
-    
+
+    const taskModalActive = ref(false)
+
+    function toggleTaskModal() {
+      taskModalActive.value = !taskModalActive.value
+      if (state.editable) state.editable = !state.editable
+    }
+
+    async function openModal(id: number) {
+      const task = await tasksStore.findTask(id)
+      
+      if(task) {
+        taskForModal.title = task.title
+        taskForModal.date = task.date
+        taskForModal.description = task.description
+        taskForModal.status = task.status
+      }
+
+      if (!taskModalActive.value) taskModalActive.value = !taskModalActive.value
+    }
+
+    function editTask() {
+      state.editable = !state.editable
+      console.log(state.editable)
+    }
+
+    async function submit(id: number) {
+      const updatedDate = state.taskEditForm.date ? state.taskEditForm.date : ''
+      tasksStore.editTask({
+        id,
+        title: state.taskEditForm.title,
+        description: state.taskEditForm.description,
+        date: updatedDate,
+      })
+      state.editable = !state.editable
+    }
+
     const filteredTasks = computed(() => {
-      // TO DO: when user clicks to complete or delete task a snackbar will appear and disappear 2s later
       // checking type and returning filtered lists
       if (props.type === 'incomplete') {
-        return tasksStore.tasks.filter(item => !isTaskLate(item.date) && item.status === 'incomplete')
+        return tasksStore.tasks.filter(
+          item => !isTaskLate(item.date) && item.status === 'incomplete'
+        )
       }
 
       if (props.type === 'late') {
-        return tasksStore.tasks.filter(item => isTaskLate(item.date) && item.status === 'incomplete')
+        return tasksStore.tasks.filter(
+          item => isTaskLate(item.date) && item.status === 'incomplete'
+        )
       }
 
       return tasksStore.tasks.filter(item => item.status === 'complete')
     })
 
-    onMounted(() => {
-      tasksStore.populateTasks()
-    })
-
-    return { tasksStore, filteredTasks, isTaskLate }
+    return {
+      capitalizeFirstLetter,
+      editTask,
+      filteredTasks,
+      formatDate,
+      isTaskLate,
+      openModal,
+      state,
+      submit,
+      taskForModal,
+      taskModalActive,
+      tasksStore,
+      timeFromNow,
+      toggleTaskModal,
+    }
   },
 })
 </script>
@@ -151,6 +304,7 @@ $black: #050505;
     background-color: #fcfcfc;
     padding: 1rem;
     margin-bottom: 1rem;
+
     & > p {
       overflow-wrap: break-word;
       hyphens: auto;
@@ -158,6 +312,7 @@ $black: #050505;
       overflow: hidden;
       text-overflow: ellipsis;
       max-height: 120px; /* max of 5 lines*/
+      cursor: pointer;
     }
 
     .complete-button {
@@ -169,6 +324,51 @@ $black: #050505;
   }
   .task:last-child {
     margin-bottom: 0;
+  }
+}
+
+@media only screen and (max-width: 690px) {
+  .task-type {
+    .tasks-list {
+      overflow-y: auto;
+      overflow-x: hidden;
+      height: calc(100% - 1rem);
+      max-height: calc(100% - 1rem);
+    }
+    & > h1 {
+      background-color: #333;
+      color: $white;
+      padding-left: 1rem;
+      border-radius: 10px;
+      margin-bottom: 1rem;
+    }
+
+    .task {
+      border-radius: 10px;
+      background-color: #fcfcfc;
+      padding: 1rem;
+      margin-bottom: 1rem;
+
+      & > p {
+        overflow-wrap: break-word;
+        hyphens: auto;
+        word-wrap: break-word;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        max-height: 120px; /* max of 5 lines*/
+        cursor: pointer;
+      }
+
+      .complete-button {
+        margin: 1rem 0 0 auto;
+      }
+
+      -webkit-box-shadow: 5px 5px 15px -2px rgba(20, 20, 20, 0.2);
+      box-shadow: 5px 5px 15px -2px rgba(20, 20, 20, 0.2);
+    }
+    .task:last-child {
+      margin-bottom: 0;
+    }
   }
 }
 </style>
