0878d023e013832cd1a3ec4973677a353998e722
Allan Kildare <allankildaredev@gmail.com>
2022-07-15 06:26:44 -0300

    refactor: conditional rendering, intro text, populate tasks in Tasks component

--- src/components/Tasks/Tasks.vue
+++ src/components/Tasks/Tasks.vue
@@ -1,40 +1,72 @@
 <template>
-  <div class="tasks" v-if="!isTasksEmpty">
+  <div 
+    v-if="tasksStore.getTasksLength > 0"
+    class="tasks"
+  >
     <list type="incomplete" />
     <list type="late" />
     <list type="complete" />
   </div>
-  <div v-else>
-    <!-- temporary text -->
-    <p>You don't have tasks yet</p>
+  <div
+    v-else
+    class="empty text-center"
+  >
+    <main class="main-intro">
+      <h1 class="text-h3">
+        Hello
+      </h1>
+      <h2 class="text-h5">
+        Welcome to To-Do App
+      </h2>
+      <p class="text-body-2 mb-3">
+        A Vue application developed by Allan Kildare
+      </p>
+      <p class="text-body-1 font-weight-medium mb-2">
+        Here you can save your tasks and complete them, as well as check if they are overdue
+      </p>
+      <p class="text-body-1 font-weight-medium">
+        You don't have tasks yet, what do you think about creating one?
+      </p>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import { useTasksStore } from '~/stores/useTasksStore'
-import List from './../List/List.vue'
+import List from '~/components/List/List.vue'
 
 export default defineComponent({
   name: 'Tasks',
   components: {
-    List,
+    List
   },
   setup() {
     const tasksStore = useTasksStore()
-    const isTasksEmpty = tasksStore.tasks.length
-    return { isTasksEmpty }
+
+    onMounted(() => {
+      tasksStore.populateTasks()
+    })
+
+    return { tasksStore }
   },
 })
 </script>
 
 <style lang="scss" scoped>
 // TO DO: responsive tasks list
-$white: #fcfcfc;
 $gray: #f3f3f5;
-$black: #050505;
-$orange: #e7883d;
-$green: #3b9d78;
+.empty {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+  .main-intro {
+    width: 60%;
+    margin-bottom: 100px; /* forcing to go up */
+  }
+}
 
 .tasks {
   height: 100vh;
@@ -48,5 +80,34 @@ $green: #3b9d78;
     width: 30%;
     height: 80vh;
   }
+}
+
+@media only screen and (max-width: 760px) and (min-width: 610px) {
+  .empty {
+  .main-intro {
+    width: 600px;
+    margin-bottom: 100px; /* forcing to go up */
+  }
+  }
+}
+
+@media only screen and (max-width: 609px) {
+  .empty {
+  .main-intro {
+    width: 300px;
+    margin-bottom: 100px; /* forcing to go up */
+  }
+  }
+}
+
+@media only screen and (max-width: 690px) {
+  .tasks {
+    flex-direction: column;
+    align-items: center;
+
+  & > div {
+    width: 90%;
+  }
+}
 }
 </style>
