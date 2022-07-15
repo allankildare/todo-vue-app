2cad0a7a47a980d53e6abbfbac452b40d2d314cf
Allan Kildare <allankildaredev@gmail.com>
2022-07-15 06:13:12 -0300

    feat: adding dayjs function to return formatted date and time from present

--- src/helpers/validateDate.ts
+++ src/helpers/validateDate.ts
@@ -1,7 +1,17 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat'
 
 dayjs.extend(relativeTime)
+dayjs.extend(LocalizedFormat)
+
+export function formatDate(date: string) {
+    return dayjs(date).format('DD/MM/YYYY [at] LT')
+}
+
+export function timeFromNow(date: string) {
+    return dayjs(date).fromNow()
+}
 
 export function isTaskLate(date: string) {
     const isLate = dayjs(date).isBefore(dayjs())
